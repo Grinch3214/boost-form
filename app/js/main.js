213945892a4f5ecc045de9d4499aba9836278d85
Max <scorpiomax90@gmail.com>
2023-04-02 21:35:50 +0300

    added easy validation

--- app/js/main.js
+++ app/js/main.js
@@ -18,21 +18,21 @@ function formSend() {
 				if(response.ok) {
 					popupSend.style.display = 'block';
 					form.reset();
-					setTimeout(() => {popupSend.style.display = 'none'}, 5000);
+					setTimeout(() => {popupSend.style.display = 'none'}, 3000);
 				} else {
 					response.json().then(data => {
 						if(Object.hasOwn(data, 'errors')) {
 							popup.innerHTML = data['errors'].map(error => error['message']).join(', ')
-							setTimeout(() => {popup.innerHTML = ''}, 5000);
+							setTimeout(() => {popup.innerHTML = ''}, 3000);
 						} else {
 							popup.innerHTML = "Oops! There was a problem submitting your form"
-							setTimeout(() => {popup.innerHTML = ''}, 5000);
+							setTimeout(() => {popup.innerHTML = ''}, 3000);
 						}
 					})
 				}
 			}).catch(error => {
 				popup.innerHTML = "Oops! There was a problem submitting your form"
-				setTimeout(() => {popup.innerHTML = ''}, 5000);
+				setTimeout(() => {popup.innerHTML = ''}, 3000);
 			});
 		}
 		form.addEventListener('submit', handleSubmit)
@@ -40,3 +40,19 @@ function formSend() {
 }
 
 formSend();
+
+(function() {
+	const emailInput = document.querySelector('.gamers__form-email input');
+	const checkboxInput = document.querySelector('.gamers__form-checked');
+	const sendBtn = document.querySelector('.gamers__form-submit');
+
+
+	const validateForm = () => {
+		(checkboxInput.checked === true  && emailInput.value !== '')
+			? sendBtn.disabled = false
+			: sendBtn.disabled = true
+	}
+
+	checkboxInput.addEventListener('change', validateForm);
+  emailInput.addEventListener('input', validateForm);
+})();
